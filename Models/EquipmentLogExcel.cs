ed1ea54a120ceed4049e029571515fd96176b4f1
timhams <timtam54@hotmail.com>
2024-03-13 21:00:28 +1000

    Style log page

--- Models/EquipmentLogExcel.cs
+++ Models/EquipmentLogExcel.cs
@@ -1,10 +1,19 @@
 ﻿namespace RoofSafety.Models
 {
-    public class EquipmentLogExcel
+
+        public class EquipmentLogExcel
     {
         public int Number { get; set; }
         public string EquipmentType_Desc { get; set; }
 
+        public string Manufacturer { get; set; }
+
+        public string WithdrawalDate { get; set; }
+
         public string SerialNo { get; set; }
+
+        public string Status { get; set; }
+        public string InspectionDate { get; set; }
+        public string InspectionDue { get; set; }
     }
 }
--- Pages/EquipmentLog.cshtml.cs
+++ Pages/EquipmentLog.cshtml.cs
@@ -14,11 +14,17 @@ namespace RoofSafety.Pages
             _context = context;
         }
         public string Error { get; set; }
-        public int InspectionID = 54;
+        [FromQuery]
+        public int InspectionID { get; set; }
         public List<EquipmentLogExcel> Equipment { get; set; }
+        public string Title { get; set; }
         public void OnGet()
         {
-            Equipment = (from ine in _context.InspEquip join eqt in _context.EquipType on ine.EquipTypeID equals eqt.id where ine.InspectionID == InspectionID select new EquipmentLogExcel { Number = ine.id, EquipmentType_Desc = eqt.EquipTypeDesc, SerialNo = ine.SerialNo}).ToList();
+            var ins = _context.Inspection.Where(i => i.id == InspectionID).FirstOrDefault();
+            var build = _context.Building.Where(i => i.id == ins.BuildingID).FirstOrDefault();
+            Title = ins.Status + " " + ins.InspectionDate.ToString("dd-MMM-yyyy") + " " + build?.BuildingName + "@" + build?.Address;
+
+            Equipment = (from ine in _context.InspEquip join eqt in _context.EquipType on ine.EquipTypeID equals eqt.id where ine.InspectionID == InspectionID select new EquipmentLogExcel { Number = ine.id, EquipmentType_Desc = eqt.EquipTypeDesc, SerialNo = ine.SerialNo, Manufacturer=ine.Manufacturer, InspectionDate=ins.InspectionDate.ToString("dd-MMM-yyyy") }).ToList();
         }
     }
 }
